49bf28a6cc0bba2f2f4a527333c484b2cad16bad
chirrtech <kipchirr.chirfestus@gmail.com>
2023-06-15 06:53:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include <stdio.h> 
+/**
+ * main - program that prints puts
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
