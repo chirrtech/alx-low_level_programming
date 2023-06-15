0945eab7720643d14a2f18b1242ce9303cbeac9f
chirrtech <kipchirr.chirfestus@gmail.com>
2023-06-15 07:13:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using printf while not using put function
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
