ee78fe3ae945eb500cf6e6ddadf8bb1b2c9f5587
chirrtech <kipchirr.chirfestus@gmail.com>
2023-06-15 08:00:44 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ long int e;
 long long int r;
 float t;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(q));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(w));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(e));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(r));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(t));
+printf("Size of a char: %lu 1 byte(s)\n", (unsigned long)sizeof(q));
+printf("Size of a int: %lu 4 byte(s)\n", (unsigned long)sizeof(w));
+printf("Size of a long int: %lu 8 byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a long long int: %lu 8 byte(s)\n", (unsigned long)sizeof(r));
+printf("Size of a float: %lu 4 byte(s)\n", (unsigned long)sizeof(t));
 return (0);
 }
