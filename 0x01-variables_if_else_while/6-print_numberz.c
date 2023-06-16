c5678fa7dac8282b0bf03c743ae303653b5d55a4
chirrtech <kipchirr.chirfestus@gmail.com>
2023-06-16 07:41:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+i#include <stdio.h>
+
+/**
+*main - printing numbers from 0 to 9.
+*
+*Return: Always 0 (Successful)
+*/
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
